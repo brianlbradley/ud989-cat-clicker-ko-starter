b7503cf36e943dcf185196124031ed6730dde09b
Brian Bradley <brianbradley@brians-air.attlocal.net>
2016-03-12 13:24:21 -0600

    Binding two functions

---
+++ js/app.js
@@ -0,0 +1,122 @@
+ var initialCats = [
+
+        {
+          clickCount: 0,
+          humanCounter: 0,
+          name: "Fluffy",
+          imgSrc: "images/Fluffy.jpeg",
+          nicknames: ['Fluffmeister', 'Fluff', 'Docile']
+
+        },
+        {
+          clickCount: 0,
+          humanCounter: 0,
+          name: "The Praying Kitty",
+          imgSrc: "images/begging.jpeg",
+          nicknames: ['Pretty Please']
+        },
+         {
+          clickCount: 0,
+          humanCounter: 0,
+          name: "Blacky",
+          imgSrc: "images/blacky.jpeg",
+          nicknames:['Darth', 'Darky','Midnight']
+        },
+         {
+          clickCount: 0,
+          humanCounter: 0,
+          name: "Cleo",
+          imgSrc: "images/Cleo.jpeg",
+          nicknames:['Cleopolis','Cleopatra']
+        },
+         {
+          clickCount: 0,
+          humanCounter: 0,
+          name: "Tabby",
+          imgSrc: "images/tabby.jpeg",
+          nicknames: [ 'Tabbatha', 'Tab', 'Tabbathina' ]
+        }
+
+      ]
+
+
+var Cat = function(data) {
+
+    this.clickCount = ko.observable(data.clickCount);
+    this.name = ko.observable(data.name);
+    this.imgSrc = ko.observable(data.imgSrc);
+    this.nicknames = ko.observable(data.nicknames);
+    this.humanCounter = ko.observable(data.humanCounter);
+
+
+
+
+    this.levels = ko.computed(function() {
+
+         if(this.clickCount()  > 80) {
+            return 'WiseOldKat';
+  	    }
+
+  	     else if (this.clickCount() >= 50) {
+  		    return 'Over the Hill';
+  		}
+
+  		else if (this.clickCount() >= 30) {
+  		    return 'Adult';
+  		}
+  		else if (this.clickCount()  >= 20) {
+  		    return 'Young Adult';
+        }
+        else if (this.clickCount()  >= 13) {
+          return 'Teenager';
+        }
+        else if (this.clickCount()  >= 10) {
+          return 'PreTeen';
+
+        }
+
+
+  	     else return 'NewBorn';
+
+  },this);
+
+}
+
+
+
+var ViewModel = function() {
+    var self = this;
+
+    this.catList = ko.observableArray([]);
+
+    initialCats.forEach(function(catItem) {
+           self.catList.push( new Cat(catItem) );
+    });
+
+	this.currentCat = ko.observable( this.catList()[0] );
+
+  this.setCat = function(click) {
+    self.currentCat(click)
+  }
+
+  //this.incCounter = function() {
+
+  //  self.currentCat().humanCounter(self.currentCat().humanCounter() +1/7);
+
+ // }
+
+	this.incrementCounter = function() {
+
+		self.currentCat().clickCount(self.currentCat().clickCount() +1);
+    self.currentCat().humanCounter(self.currentCat().humanCounter() + 1/7);
+		//former  this.clickcount(this.clickcount() + 1)
+		//var count = 0;
+		//count ++;
+	}
+
+	};
+
+
+
+
+ko.applyBindings(new ViewModel());
